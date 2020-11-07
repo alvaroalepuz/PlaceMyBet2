176e8de8a3c9cefe159c07dc7895970248189d90
Alvaro Alepuz <alvaroalepuz99@gmail.com>
2020-11-07 16:59:28 +0100

    Arreglando bd local

---
+++ PlaceMyBet/Controllers/ApuestasController.cs
@@ -0,0 +1,69 @@
+﻿using PlaceMyBet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlaceMyBet.Controllers
+{
+    public class ApuestasController : ApiController
+    {
+        // GET: api/Apuestas
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        //GET: api/Apuestas/1
+        public apuestaDto3 Get(int id)
+        {
+            var repo = new apuestaRepository();
+            apuestaDto3 e = repo.RetriveFiltro3();
+            return e;
+        }
+        //GET: api/Apuestas?TipoMercado,CuotaUnder,CuotaOver
+        [Authorize(Roles = "Admin")]
+        //public apuestaDto2 Get(int id)
+        //{
+        //    var repo = new apuestaRepository();
+        //    apuestaDto2 e = repo.RetriveFiltro();
+        //    return e;
+        //}
+
+        //GET: api/Apuestas?TipoMercado,CuotaUnder,CuotaOver
+        //[Authorize(Roles ="Admin")]
+        //public apuestaDto Get(int id)
+        //{
+        //    var repo = new apuestaRepository();
+        //    apuestaDto e = repo.RetriveFiltrado();
+        //    return e;
+        //}
+
+        // GET: api/Apuestas/5
+        //public apuesta Get(int id)
+        //{
+        //    var repo = new apuestaRepository();
+        //    apuesta a = repo.Retrive();
+        //    return a;
+        //}
+
+        // POST: api/Apuestas
+        [Authorize]
+        public void Post([FromBody]apuesta apuesta)
+        {
+            var repo = new apuestaRepository();
+            repo.Save(apuesta);
+        }
+
+        // PUT: api/Apuestas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Apuestas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PlaceMyBet/Controllers/MercadosController.cs
@@ -0,0 +1,56 @@
+﻿using PlaceMyBet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlaceMyBet.Controllers
+{
+    public class MercadosController : ApiController
+    {
+        // GET: api/Mercados
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        //GET: api/Mercados?TipoMercado,CuotaUnder,CuotaOver
+        //public mercadoDto Get(int id)
+        //{
+        //    var repo = new mercadoRepository();
+        //    mercadoDto e = repo.RetriveTipoMercado();
+        //    return e;
+        //}
+        //GET: api/Mercados/2
+        
+        public mercado Get(int id)  
+        {
+            var repo = new mercadoRepository();
+            mercado e = repo.RetriveFiltrado();
+            return e;
+        }
+        // GET: api/Mercados/5
+        //public mercado Get(int id)
+        //{
+        //    var repo = new mercadoRepository();
+        //    mercado m = repo.Retrive();
+        //    return m;
+        //}
+
+        // POST: api/Mercados
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Mercados/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Mercados/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
